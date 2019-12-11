3234bd4f2f8d61d316b0c5cceff6c2c6a9f47a76
ison <isonz@qq.com>
2019-12-11 18:27:03 +0800

    asdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Inject, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -27,17 +28,47 @@ export function HttpLoaderFactory(http: HttpClient) {
       }
     })
   ],
-  providers: [],
+  providers: [
+    {provide: 'LANG_LIST', useValue: ['en-us', 'zh-cn', 'zh-hk']},
+    {provide: 'LANG_DEFAULT', useValue: 'en-us'}
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {
 
-  constructor(translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
+  constructor(
+    public translate: TranslateService,
+    public route: ActivatedRoute,
+    @Inject('LANG_LIST') public langList: Array<string>,
+    @Inject('LANG_DEFAULT') public langDefault: string
+  ) {
 
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('cn');
+    this.getLang();
+
+  }
+
+  getLang() {
+    let lang: any = navigator.language.toLowerCase();
+    this.route.queryParamMap.subscribe(params => {
+      const l: string = params.get('lang');
+      if (l) { lang = l; }
+
+      console.log(lang);
+
+
+    });
+    this.setLang(lang);
+  }
+
+  setLang(lang: string) {
+    console.log(lang);
+    if (this.langList.indexOf(lang) > -1) {
+      // translate.setDefaultLang(lang);
+      this.translate.use(lang);
+    } else {
+      this.translate.use(this.langDefault);
+    }
   }
 
 }
